fbfe2b7da71b8191e49a33503d7cef1e28213e42
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2023-07-15 20:03:04 +0200

    Setting up foundation of project & removing unnecessary things

--- ChatApplicationSignalR/Program.cs
+++ ChatApplicationSignalR/Program.cs
@@ -1,4 +1,3 @@
-using ChatApplicationSignalR.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
