298e79cd9258f298f1463bc9f8ee7eff492b5325
Mozin0 <malshalan65@gmail.com>
2023-08-13 02:44:01 +0200

    Migrations Update

---
+++ ChatApplicationSignalR/Migrations/20230807003909_ConversationsUpdate.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApplicationSignalR.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConversationsUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConversationUser_Conversation_ConversationsConversationId",
+                table: "ConversationUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Conversation_ConversationId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Conversation",
+                table: "Conversation");
+
+            migrationBuilder.RenameTable(
+                name: "Conversation",
+                newName: "Conversations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Conversations",
+                table: "Conversations",
+                column: "ConversationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConversationUser_Conversations_ConversationsConversationId",
+                table: "ConversationUser",
+                column: "ConversationsConversationId",
+                principalTable: "Conversations",
+                principalColumn: "ConversationId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Conversations_ConversationId",
+                table: "Messages",
+                column: "ConversationId",
+                principalTable: "Conversations",
+                principalColumn: "ConversationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ConversationUser_Conversations_ConversationsConversationId",
+                table: "ConversationUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Conversations_ConversationId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Conversations",
+                table: "Conversations");
+
+            migrationBuilder.RenameTable(
+                name: "Conversations",
+                newName: "Conversation");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Conversation",
+                table: "Conversation",
+                column: "ConversationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ConversationUser_Conversation_ConversationsConversationId",
+                table: "ConversationUser",
+                column: "ConversationsConversationId",
+                principalTable: "Conversation",
+                principalColumn: "ConversationId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Conversation_ConversationId",
+                table: "Messages",
+                column: "ConversationId",
+                principalTable: "Conversation",
+                principalColumn: "ConversationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
