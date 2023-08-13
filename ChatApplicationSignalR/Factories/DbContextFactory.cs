2978f2b3a4eb94a316b2837bdda78252ae302d5a
Mozin0 <malshalan65@gmail.com>
2023-08-13 02:41:39 +0200

    Added DbContextFactory: Ensuring proper management of DbContext lifetimes and preventing threading issues

---
+++ ChatApplicationSignalR/Factories/DbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatApplicationSignalR.Factories
+{
+    public class DbContextFactory 
+    {
+        private IConfiguration _configuration;
+
+        public DbContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ChatApplicationDbContext CreateDbContext()
+        {
+            var optionBuilder = new DbContextOptionsBuilder<ChatApplicationDbContext>();
+            optionBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+            return new ChatApplicationDbContext(optionBuilder.Options);
+        }
+    }
+}
